b4fe5a536da67dd0cda21975d78dece0dcd30691
Denzell Griffith <grift210@gmail.com>
2025-08-16 01:46:07 -0600

    feat: server running in main file

--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"ecommerce/go/chatbot/server"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("Hello there")
+	if err := godotenv.Load(".env"); err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	PORT := os.Getenv("PORT")
+	DATABASE_URL := os.Getenv("DATABASE_URL")
+
+	if PORT == "" {
+		log.Fatal("port is required")
+	}
+
+	if DATABASE_URL == "" {
+		log.Fatal("database url is required")
+	}
+
+	server.NewServer(&server.Config{
+		Port:        PORT,
+		DatabaseUrl: DATABASE_URL,
+	})
 }
