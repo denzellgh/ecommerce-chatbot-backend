b011d3cdcdfb088af2f31556657ac3a2d7766ac5
Denzell Griffith <grift210@gmail.com>
2025-08-17 02:42:34 -0600

    feat: new env added to run api

--- main.go
+++ main.go
@@ -15,6 +15,8 @@ func main() {
 
 	PORT := os.Getenv("PORT")
 	DATABASE_URL := os.Getenv("DATABASE_URL")
+	OLLAMA_URL := os.Getenv("OLLAMA_URL")
+	MODEL_NAME := os.Getenv("MODEL_NAME")
 
 	if PORT == "" {
 		log.Fatal("port is required")
@@ -24,8 +26,18 @@ func main() {
 		log.Fatal("database url is required")
 	}
 
+	if OLLAMA_URL == "" {
+		OLLAMA_URL = "http://localhost:11434"
+	}
+
+	if MODEL_NAME == "" {
+		log.Fatal("model name is required")
+	}
+
 	server.NewServer(&server.Config{
 		Port:        PORT,
 		DatabaseUrl: DATABASE_URL,
+		OllamaUrl:   OLLAMA_URL,
+		ModelName:   MODEL_NAME,
 	})
 }
