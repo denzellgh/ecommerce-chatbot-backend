343c3347de8dc9d29d8625df3a82d9d214e79cd4
Denzell Griffith <grift210@gmail.com>
2025-08-17 12:19:46 -0600

    fix: changing type response

--- models/api.go
+++ models/api.go
@@ -4,5 +4,12 @@ type ApiResponse struct {
 	Data       any    `json:"data"`
 	StatusCode int    `json:"statusCode,omitempty"`
 	Message    string `json:"message,omitempty"`
-	Error      string `json:"error,omitempty"`
+	Success    bool   `json:"success,omitempty"`
+}
+
+type ApiResponseGeneric[T any] struct {
+	Data       *T     `json:"data"`
+	StatusCode int    `json:"statusCode,omitempty"`
+	Message    string `json:"message,omitempty"`
+	Success    bool   `json:"success,omitempty"`
 }
