8e79699de55a714793d19628c09e50c9286b3b9d
Denzell Griffith <grift210@gmail.com>
2025-08-17 15:21:09 -0600

    fix: updates in models

--- models/products.go
+++ models/products.go
@@ -3,15 +3,15 @@ package models
 import "time"
 
 type Products struct {
-	Id             string    `json:"id"`
-	Name           string    `json:"name"`
-	Brand          string    `json:"brand"`
-	Category_Id    string    `json:"category_id"`
-	Price          float64   `json:"price"`
-	Stock_Quantity int       `json:"stock_quantity"`
-	Description    string    `json:"description"`
-	Specs          string    `json:"specs"` // Warning
-	CreatedAt      time.Time `json:"created_at"`
-	UpdatedAt      time.Time `json:"updated_at"`
-	DeletedAt      time.Time `json:"deleted_at"`
+	ID            string    `json:"id"`
+	Name          string    `json:"name"`
+	Brand         string    `json:"brand"`
+	CategoryID    string    `json:"category_id"`
+	Price         float64   `json:"price"`
+	StockQuantity int       `json:"stock_quantity"`
+	Description   string    `json:"description"`
+	Specs         string    `json:"specs"` // Warning
+	CreatedAt     time.Time `json:"created_at"`
+	UpdatedAt     time.Time `json:"updated_at"`
+	DeletedAt     time.Time `json:"deleted_at"`
 }
