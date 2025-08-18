9a40b90fd8a647ded67ea1880c11199ae9ddd46e
Denzell Griffith <grift210@gmail.com>
2025-08-17 22:53:33 -0600

    fix: cors error

--- middleware/cors.go
+++ middleware/cors.go
@@ -8,9 +8,9 @@ import (
 
 func CORS() func(http.Handler) http.Handler {
 	return cors.Handler(cors.Options{
-		AllowedOrigins:   []string{"https://*", "http://*", "http://localhost:4000"},
+		AllowedOrigins:   []string{"https://*", "http://*", "http://localhost:3000"},
 		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE"},
-		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token", "X-User-Session"},
 		AllowCredentials: true,
 	})
 }
